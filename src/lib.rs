da5d72ef46172da06b002facc4e546aa523443c6
Gabe Venberg <gabevenberg@gmail.com>
2026-07-02 10:02:38 +0000

    Constructed hashmap of the various types in the module and made error handling a lot simpler.

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,44 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use std::{env, fs, path::PathBuf};
-use syn::{ItemMod, LitStr, parse_macro_input};
+use std::{collections::HashMap, env, fs, path::PathBuf};
+use syn::{Item, ItemEnum, ItemMod, ItemStruct, LitStr, parse2, spanned::Spanned};
 
+//the inner function just lets us return a result, and then have the error case of that result
+//turned into a neat compiler error.
 #[proc_macro_attribute]
 pub fn from_toml(args: TokenStream, input: TokenStream) -> TokenStream {
-    let path = parse_macro_input!(args as LitStr);
-    let module = parse_macro_input!(input as ItemMod);
+    from_toml_inner(args.into(), input.into())
+        .unwrap_or_else(|e| e.to_compile_error())
+        .into()
+}
+
+fn from_toml_inner(
+    args: proc_macro2::TokenStream,
+    input: proc_macro2::TokenStream,
+) -> Result<proc_macro2::TokenStream, syn::Error> {
+    let path: LitStr = parse2(args)?;
+    let module: ItemMod = parse2(input)?;
 
     eprintln!("{}", path.value());
     eprintln!("{:#?}", module);
 
-    let toml_path = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap()).join(path.value());
-    let content = fs::read_to_string(&toml_path).unwrap();
-    let toml_value: toml::Value = toml::from_str(&content).unwrap();
+    //parse the toml, returning any errors in opening the file or parsing as compile errors
+    let toml_path =
+        PathBuf::from(env::var("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR not set"))
+            .join(path.value());
+    let content =
+        fs::read_to_string(&toml_path).map_err(|e| syn::Error::new(path.span(), e.to_string()))?;
+    let toml_value: toml::Value =
+        toml::from_str(&content).map_err(|e| syn::Error::new(path.span(), e.to_string()))?;
     eprintln!("{:#?}", toml_value);
 
-    let items = &module.content.unwrap().1;
-    let (root_structs, other_structs): (Vec<_>, Vec<_>) = items.iter().partition(|item| {
-        if let syn::Item::Struct(s) = item {
+    let items = &module
+        .content
+        .as_ref()
+        .ok_or_else(|| syn::Error::new(module.span(), "Module must have content"))?
+        .1;
+    let (root_structs, other_items): (Vec<_>, Vec<_>) = items.iter().partition(|item| {
+        if let Item::Struct(s) = item {
             s.attrs.iter().any(|a| a.path().is_ident("root"))
         } else {
             false
@@ -26,7 +46,7 @@ pub fn from_toml(args: TokenStream, input: TokenStream) -> TokenStream {
     });
 
     let root_struct = if root_structs.len() == 1 {
-        if let syn::Item::Struct(s) = root_structs[0] {
+        if let Item::Struct(s) = root_structs[0] {
             let mut s = s.clone();
             s.attrs.retain(|a| !a.path().is_ident("root"));
             s
@@ -34,11 +54,41 @@ pub fn from_toml(args: TokenStream, input: TokenStream) -> TokenStream {
             unreachable!()
         }
     } else {
-        panic!("Must have only one '#[root]' struct!")
+        return Err(syn::Error::new(
+            module.span(),
+            "Must have exactly one struct with `#[root] in the config module.",
+        ));
     };
 
+    eprintln!("{:#?}", other_items);
     eprintln!("{:#?}", root_struct);
-    eprintln!("{:#?}", other_structs);
+
+    let mut defs: HashMap<String, TypeDef> = HashMap::new();
+    for item in other_items {
+        match item {
+            Item::Struct(s) => {
+                defs.insert(s.ident.to_string(), TypeDef::Struct(s));
+            }
+            Item::Enum(e) => {
+                defs.insert(e.ident.to_string(), TypeDef::Enum(e));
+            }
+            i => {
+                return Err(syn::Error::new(
+                    i.span(),
+                    "Can only have structs and enums in the toml_config module.",
+                ));
+            }
+        }
+    }
+    defs.insert(root_struct.ident.to_string(), TypeDef::Struct(&root_struct));
+
+    eprintln!("{:#?}", defs);
 
     todo!()
+}
+
+#[derive(Debug)]
+enum TypeDef<'a> {
+    Struct(&'a ItemStruct),
+    Enum(&'a ItemEnum),
 }
